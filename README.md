d43e3b43f28575cc17a5295c9413763809690800
James Hodgkinson <james@terminaloutcomes.com>
2021-12-13 08:59:36 +1000

    making it much more configurable and usable

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # telegrab
 
 A tool for downloading files from telegram channels.
+
+
+## Configuration
+
+It's a JSON file in `~/.config/telegrab.json` or your OS equivalent.
+
+Generate a session id using something like `openssl rand -hex 32` - keeping it stable means you don't have to log in each time.
+
+```json
+{
+"session_id" : "asdfasdfasfsfasd",
+"api_id" : "123456",
+"api_hash" : "asdfasdfasdfasdf"
+}
+
+```
+
+You specify the `download_dir` in config or on the command line (with `--download-dir`).
--- telegrab/__main__.py
+++ telegrab/__main__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import json
+from json.decoder import JSONDecodeError
 import os
 import sys
 
@@ -40,15 +41,62 @@ def select_channel(channel_name: str, telegram_client: TelegramClient, debug: bo
     return selected_chat
 
 
+def load_config():
+    """
+    loads configuration things
+    """
+    homedir = os.path.expanduser('~/.config/')
+    config_filename = f"{homedir}telegrab.json"
+    if not os.path.exists(config_filename):
+        print(f"Unable to find config file, looked in : {config_filename}")
+        return False
+    with open(config_filename, 'r') as file_handle:
+        try:
+            config = json.load(file_handle)
+        except JSONDecodeError as error_message:
+            print(f"Failed to parse {config_filename}: {error_message}")
+            return False
+
+    missing_vars = []
+    failed = False
+    for var in ("session_id", "api_hash", "api_id"): #"download_dir",
+        if not config.get(var):
+            failed=True
+            missing_vars.append(var)
+    if failed:
+        print(f"Missing config fars in {config_filename}, please configure the following: {','.join(missing_vars)}")
+        return False
+    return config
+
 @click.option("-d", "--debug", is_flag=True, default=False)
 @click.option("-l", "--list-chats", type=bool, is_flag=True, default=False)
 @click.option("--channel", default="")
+@click.option("-o","--download-dir", type=click.Path(exists=True, allow_dash=False, writable=True, file_okay=False, dir_okay=True), )
 @click.command()
-def cli(channel: str, list_chats: bool, debug: bool):
+def cli(channel: str, list_chats: bool, debug: bool, download_dir: str):
+    """ main cli interface """
+    config = load_config()
+    if not config:
+        return False
+
+    if not config.get("download_dir", download_dir):
+        print("Please specify a download dir in config or command line options.")
+        return False
+    if debug:
+        print(f"Download dir: {download_dir}")
+
+    download_dir = os.path.expanduser(config.get("download_dir", download_dir))
+    if not download_dir.endswith("/"):
+        download_dir = f"{download_dir}/"
+
+    if not os.path.exists(download_dir):
+        print(f"The downloads dir {download_dir} does not exist, please create it.")
+        return False
+
     with TelegramClient(
-        session=session_id,
-        api_id=api_id,
-        api_hash=api_hash,
+        session=config.get("session_id"),
+        api_id=config.get("api_id"),
+        api_hash=config.get("api_hash"),
         request_retries=5,
         connection_retries=5,
         retry_delay=30,
@@ -56,11 +104,11 @@ def cli(channel: str, list_chats: bool, debug: bool):
     ) as client:
 
         # grab channels
-
         selected_chat = select_channel(channel, client, debug, list_chats=True)
         if list_chats:
             return True
 
+        # prompt the user for a channel
         while not selected_chat:
             channel = click.prompt("Please enter a channel")
             selected_chat = select_channel(channel.strip(), client, debug, list_chats=False)
@@ -68,13 +116,12 @@ def cli(channel: str, list_chats: bool, debug: bool):
 
         for messagedata in client.iter_messages(
             entity=selected_chat.entity,
-            # limit=1,
         ):
             message = messagedata.to_dict()
 
             if message.get("media"):
                 media = message.get("media")
-                # print("Found an image")
+                if debug: print("Found an image")
                 attributes = media.get("document", {}).get("attributes", {})
                 filename = False
                 for att in attributes:
@@ -88,7 +135,7 @@ def cli(channel: str, list_chats: bool, debug: bool):
                         print(json.dumps(message, default=str))
                     continue
                 print(f"Filename: {filename}")
-                download_filename = f"downloads/{filename}"
+                download_filename = f"{download_dir}{filename}"
                 if os.path.exists(download_filename):
                     print(f"Skipping {filename}")
                     if not debug:
@@ -102,7 +149,7 @@ def cli(channel: str, list_chats: bool, debug: bool):
                         .lower()
                         == "y"
                     ):
-                        download_filename = f"downloads/{message.get('id')}-{filename}"
+                        download_filename = f"{download_dir}{message.get('id')}-{filename}"
                         if os.path.exists(download_filename):
                             print(f"Skipping {filename}")
                             continue
@@ -121,7 +168,7 @@ def cli(channel: str, list_chats: bool, debug: bool):
                     os.remove(download_filename)
                     sys.exit()
 
-            # print(json.dumps(message, indent=4,default=str))
+            if debug: print(json.dumps(message, indent=4,default=str))
 
 
 if __name__ == "__main__":
